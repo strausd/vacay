486867030e2c5b5bce8aa05542260abbf5a4c31b
Danny Straus <strausd@me.com>
2018-03-08 08:28:50 -0600

    Hide elements of the header based on auth status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import UserMenu from './UserMenu';
 
 
-const Header = () => {
-    return (
-        <div>
-            <ul>
-                <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
-                <li><NavLink exact to="/pricing" activeClassName="active">Pricing</NavLink></li>
-                <li><NavLink exact to="/contact" activeClassName="active">Contact</NavLink></li>
-                <li><NavLink exact to="/signup" activeClassName="active">Signup</NavLink></li>
-                <li><NavLink exact to="/login" activeClassName="active">Login</NavLink></li>
-                <li><NavLink exact to="/dashboard" activeClassName="active">Dashboard</NavLink></li>
-            </ul>
-            <UserMenu />
-        </div>
-    );
+class Header extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li><NavLink exact to="/" activeClassName="active">Home</NavLink></li>
+                    <li><NavLink exact to="/pricing" activeClassName="active">Pricing</NavLink></li>
+                    <li><NavLink exact to="/contact" activeClassName="active">Contact</NavLink></li>
+                    {this.props.isAuthenticated ? null : <li><NavLink exact to="/signup" activeClassName="active">Signup</NavLink></li>}
+                    {this.props.isAuthenticated ? null : <li><NavLink exact to="/login" activeClassName="active">Login</NavLink></li>}
+                    {!this.props.isAuthenticated ? null : <li><NavLink exact to="/dashboard" activeClassName="active">Dashboard</NavLink></li>}
+                </ul>
+                <UserMenu />
+            </div>
+        );
+    }
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.jwt ? true : false
+    };
+};
+
+export default connect(mapStateToProps)(Header);
