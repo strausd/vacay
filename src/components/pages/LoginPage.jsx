9a58b3fe086b84e06627cfd739be0be9258eb2c2
Danny Straus <strausd@me.com>
2018-04-09 08:22:56 -0500

    Work on styling forms

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -36,23 +36,23 @@ export class LoginPage extends React.Component {
     render() {
         return (
             <div className="container page-content">
-                <h1><i className="icon ion-home"></i>Login</h1>
-                <form onSubmit={this.handleLoginSubmit}>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" ref="email" placeholder="email" required />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" ref="password" placeholder="password" minLength="6" required />
-                    </div>
-                    <div>
-                        <input type="submit" value="Login" />
-                    </div>
-                </form>
-                <Link to="/forgotpassword">
-                    Forgot Password?
-                </Link>
+                <div className="center-form">
+                    <h1><i className="icon ion-home"></i>Login</h1>
+                    <form onSubmit={this.handleLoginSubmit}>
+                        <div className="input-group">
+                            <input type="email" id="email" name="email" ref="email" placeholder="Email" required />
+                        </div>
+                        <div className="input-group">
+                            <input type="password" id="password" name="password" ref="password" placeholder="Password" minLength="6" required />
+                        </div>
+                        <div>
+                            <input className="btn block" type="submit" value="Login" />
+                        </div>
+                    </form>
+                    <Link to="/forgotpassword">
+                        Forgot Password?
+                    </Link>
+                </div>
             </div>
         );
     }
--- src/components/pages/RegisterPage.jsx
+++ src/components/pages/RegisterPage.jsx
@@ -41,34 +41,31 @@ export class RegisterPage extends React.Component {
     render = () => {
         return (
             <div className="container page-content">
-                <h1>Signup</h1>
-                <form onSubmit={this.handleSignupSubmit}>
-                    <p className="hidden" ref="error_container"></p>
-                    <div>
-                        <label htmlFor="first_name">First Name</label>
-                        <input type="text" id="first_name" name="first_name" ref="first_name" placeholder="John" required />
-                    </div>
-                    <div>
-                        <label htmlFor="last_name">Last Name</label>
-                        <input type="text" id="last_name" name="last_name" ref="last_name" placeholder="Doe" required />
-                    </div>
+                <div className="center-form">
+                    <h1>Register</h1>
+                    <form onSubmit={this.handleSignupSubmit}>
+                        <p className="hidden" ref="error_container"></p>
+                        <div className="input-group">
+                            <input type="text" id="first_name" name="first_name" ref="first_name" placeholder="First Name" required />
+                        </div>
+                        <div className="input-group">
+                            <input type="text" id="last_name" name="last_name" ref="last_name" placeholder="Last Name" required />
+                        </div>
 
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" ref="email" placeholder="email" required />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" ref="password" placeholder="password" minLength="6" required />
-                    </div>
-                    <div>
-                        <label htmlFor="password_confirm">Confirm Password</label>
-                        <input type="password" id="password_confirm" name="password_confirm" ref="password_confirm" placeholder="confirm password" minLength="6" required />
-                    </div>
-                    <div>
-                        <input type="submit" value="Signup" />
-                    </div>
-                </form>
+                        <div className="input-group">
+                            <input type="email" id="email" name="email" ref="email" placeholder="Email" required />
+                        </div>
+                        <div className="input-group">
+                            <input type="password" id="password" name="password" ref="password" placeholder="Password" minLength="6" required />
+                        </div>
+                        <div className="input-group">
+                            <input type="password" id="password_confirm" name="password_confirm" ref="password_confirm" placeholder="Confirm Password" minLength="6" required />
+                        </div>
+                        <div>
+                            <input className="btn block" type="submit" value="Signup" />
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
